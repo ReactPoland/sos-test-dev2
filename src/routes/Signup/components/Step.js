37f4698739cacc9ee4f361155d693948d39da205
Daniel Kanas <dkanas95@gmail.com>
2017-03-31 14:04:08 +0200

    step component

---
+++ src/routes/Signup/components/Step.js
@@ -0,0 +1,55 @@
+import React, { PropTypes } from 'react'
+import ProgressBar from './ProgressBar'
+
+import './Step.scss'
+
+// since those components are very specific for the Step component, they are declared in the same file
+const BackButton = ({ onClick, visible }) => visible ? <button onClick={onClick}>Back</button> : null
+BackButton.propTypes = {
+  onClick: PropTypes.func,
+  visible: PropTypes.bool
+}
+
+const NextButton = ({ onClick, visible }) => visible ? <button onClick={onClick}>Next</button> : null
+NextButton.propTypes = {
+  onClick: PropTypes.func,
+  visible: PropTypes.bool
+}
+
+const Buttons = ({ showBack, onBack, showNext, onNext }) => (
+  <div>
+    <BackButton visible={showBack} onClick={onBack} />
+    <NextButton visible={showNext} onClick={onNext} />
+  </div>
+)
+Buttons.propTypes = {
+  showBack: PropTypes.bool,
+  showNext: PropTypes.bool,
+  onBack: PropTypes.func,
+  onNext: PropTypes.func
+}
+
+const Step = ({ label, progress, children, showNext, showBack, onNext, onBack }) => (
+  <div>
+    <h1 className='step--label'>{label}</h1>
+    <ProgressBar progress={progress || 30} />
+    {children}
+    <Buttons
+      showBack={showBack}
+      showNext={showNext}
+      onBack={onBack}
+      onNext={onNext} />
+  </div>
+)
+
+Step.propTypes = {
+  showBack: PropTypes.bool,
+  showNext: PropTypes.bool,
+  onBack: PropTypes.func,
+  onNext: PropTypes.func,
+  label: PropTypes.string,
+  progress: PropTypes.number
+}
+
+export default Step
+

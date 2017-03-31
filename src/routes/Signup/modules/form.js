4e994eb2ea25b474381344b89269ce5c59070f08
Daniel Kanas <dkanas95@gmail.com>
2017-03-31 14:05:20 +0200

    validations and changing steps feature in form module

--- src/routes/Signup/modules/form.js
+++ src/routes/Signup/modules/form.js
@@ -1,6 +1,18 @@
+// helpers
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const validators = {
+  email: value => {
+    if (value === '') return 'Email is required'
+    else return emailRegex.test(value) ? '' : 'Incorrect email'
+  },
+  password: value => value.length < 6 ? 'Password is required and needs to be at least 6 characters long' : '',
+  confirmPassword: (value, { password }) => value !== password ? 'Passwords don\'t match' : ''
+}
+
 // constants
 const SET_INPUT_VALUE = 'SET_INPUT_VALUE'
 const SET_VALIDATION_ERROR = 'SET_VALIDATION_ERROR'
+const CHANGE_STEP = 'CHANGE_STEP'
 
 // action creators
 export const setInputValue = (inputName, value) => ({
@@ -15,6 +27,37 @@ export const setValidationError = (inputName, error) => ({
   error
 })
 
+export const changeStep = step => ({
+  type: CHANGE_STEP,
+  step
+})
+
+// thunks
+export const handleInput = (inputName, value) => (dispatch, getState) => {
+  const { inputValues } = getState().form
+  const validator = validators[inputName]
+  const error = validator ? validator(value, inputValues) : ''
+  dispatch(setInputValue(inputName, value))
+  dispatch(setValidationError(inputName, error))
+}
+
+export const validateStep = () => (dispatch, getState) => {
+  const { step, inputValues } = getState().form
+  const handlers = {
+    0: (inputValues) => {
+      // return a new array of key - validation result values: [input name, validation error]
+      const validations = ['email', 'password', 'confirmPassword'].map(
+        inputName => [inputName, validators[inputName](inputValues[inputName], inputValues)]
+      )
+      const errors = validations.filter(([_, value]) => value !== '')
+      if (errors.length === 0) dispatch(changeStep(1))
+      else errors.forEach(data => dispatch(setValidationError(...data)))
+    }
+  }
+  const handler = handlers[step]
+  if (handler) handler(inputValues)
+}
+
 // action handlers
 const handlers = {
   [SET_INPUT_VALUE]: (state, { inputName, value }) => {
@@ -25,6 +68,7 @@ const handlers = {
     const validationErrors = { ...state.validationErrors, [inputName]: error }
     return { ...state, validationErrors }
   },
+  [CHANGE_STEP]: (state, { step }) => ({ ...state, step })
 }
 
 // reducer
@@ -34,7 +78,7 @@ const initialState = {
   inputValues: {
     email: '',
     password: '',
-    passwordConfirmation: '',
+    confirmPassword: '',
     dayOfBirth: '',
     monthOfBirth: '',
     yearOfBirth: '',
@@ -44,7 +88,7 @@ const initialState = {
   validationErrors: {
     email: '',
     password: '',
-    passwordConfirmation: '',
+    confirmPassword: '',
     dayOfBirth: '',
     monthOfBirth: '',
     yearOfBirth: '',
@@ -53,6 +97,7 @@ const initialState = {
   }
 }
 
+// named function for easier debugging
 export default function formReducer (state = initialState, action) {
   const handler = handlers[action.type]
 

98118dee6bc4c1f9ca3e5626ea6d94cf53f9f266
Daniel Kanas <dkanas95@gmail.com>
2017-03-31 22:52:04 +0200

    added last step to signup component and updated styling

--- src/routes/Signup/components/Signup.js
+++ src/routes/Signup/components/Signup.js
@@ -2,9 +2,10 @@ import React from 'react'
 import ProgressBar from './ProgressBar'
 import Step1 from './Step1'
 import Step2 from './Step2'
+import Step3 from './Step3'
 import './Signup.scss'
 
-const steps = [Step1, Step2]
+const steps = [Step1, Step2, Step3]
 const stepLabels = ['Signup', 'Signup', 'Thank you!']
 
 export const Signup = ({ step, progress, ...propsToPass }) => {

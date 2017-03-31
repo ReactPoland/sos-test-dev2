bc9cf6a95841c7cd298fea1c36de0309693c95e0
Daniel Kanas <dkanas95@gmail.com>
2017-03-31 14:08:28 +0200

    first step

---
+++ src/routes/Signup/components/Step1.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Step from './Step'
+import TextInput from './TextInput'
+
+const Step1 = ({ handleInput, onNext, inputValues, validationErrors, progress }) => (
+  <Step
+    label='Signup'
+    showBack={false}
+    showNext
+    onNext={onNext}
+    progress={progress}>
+    <TextInput
+      type='email'
+      onChange={handleInput('email')}
+      onBlur={handleInput('email')}
+      value={inputValues.email}
+      error={validationErrors.email}
+      label='Email' />
+    <TextInput
+      type='password'
+      onChange={handleInput('password')}
+      onBlur={handleInput('password')}
+      value={inputValues.password}
+      error={validationErrors.password}
+      label='Password' />
+    <TextInput
+      type='password'
+      onChange={handleInput('confirmPassword')}
+      onBlur={handleInput('confirmPassword')}
+      value={inputValues.confirmPassword}
+      error={validationErrors.confirmPassword}
+      label='Confirm password' />
+  </Step>
+)
+
+export default Step1
